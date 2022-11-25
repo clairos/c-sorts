f549bc8fc43d3992b09cd629c667158e8538d431
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-11-25 15:39:52 -0300

    anotações selection

--- selection.c
+++ selection.c
@@ -5,20 +5,23 @@ void selection_sort(int arr[], int tam)
 {
     int posMenor, i, j, penultimo, aux;
     penultimo = tam - 1;
+    // faz a troca para todas as posições menos a última
+    // isso pq o maior valor ja vai estar correto na ultima posição depois das trocas
 
     for (i = 0; i < penultimo; i++)
     {
-        posMenor = i;
-        for (j = i + 1; j < tam; j++)
-        {
-            if (arr[j] < arr[posMenor])
+        posMenor = i; // a menor posição é aquela que vc já está (i)
+        // procura o menor elemento em relação a i
+        for (j = i + 1; j < tam; j++) // vai desde a próxima posição até o final do vetor
+        { 
+            if (arr[j] < arr[posMenor]) // se a posição j for menor que a posição menor, faz a troca
             {
                 posMenor = j;
             }
         }
 
         if (i != posMenor)
-        {
+        { // se i for diferente da menor, troca os elementos utilizando um auxiliar
             aux = arr[i];
             arr[i] = arr[posMenor];
             arr[posMenor] = aux;
