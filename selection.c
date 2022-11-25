2de031678d61a925cb807aff3ad203322d99c55c
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-11-25 15:32:25 -0300

    condicao extra selection sort + indent

--- selection.c
+++ selection.c
@@ -4,22 +4,25 @@
 void selection_sort(int arr[], int tam)
 {
     int posMenor, i, j, penultimo, aux;
-    penultimo = tam-1;
+    penultimo = tam - 1;
 
-    for(i=0; i<penultimo; i++)
+    for (i = 0; i < penultimo; i++)
     {
         posMenor = i;
-        for(j=i+1; j<tam; j++)
+        for (j = i + 1; j < tam; j++)
         {
-            if(arr[j]<arr[posMenor])
+            if (arr[j] < arr[posMenor])
             {
                 posMenor = j;
             }
         }
 
-        aux = arr[i];
-        arr[i] = arr[posMenor];
-        arr[posMenor] = aux;
+        if (i != posMenor)
+        {
+            aux = arr[i];
+            arr[i] = arr[posMenor];
+            arr[posMenor] = aux;
+        }
     }
 }
 
@@ -27,7 +30,7 @@ int main()
 {
     int i, tam = 5, arr[5] = {7, 19, 3, -5, 2};
 
-    for (i=0; i<tam; i++)
+    for (i = 0; i < tam; i++)
     {
         printf("arr[%d]=%d\n", i, arr[i]); // printa somente o array normal
     }
@@ -35,9 +38,9 @@ int main()
     selection_sort(arr, tam); // chama a funcao para executar o SS
     printf("\n-----------------------------\n");
 
-    for (i=0; i<tam; i++)
+    for (i = 0; i < tam; i++)
     {
-        printf("arr[%d]=%d\n", i, arr[i]); 
+        printf("arr[%d]=%d\n", i, arr[i]);
     }
 
     return 0;
